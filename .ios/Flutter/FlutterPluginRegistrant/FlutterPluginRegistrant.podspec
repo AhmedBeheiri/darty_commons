e389454930feffa8a16ea0c78a257084ff585876
ahmed_beheiri <ahmedbeheirii@gmail.com>
2020-08-04 02:05:59 +0200

    dart commons 1.0.0

--- .ios/Flutter/FlutterPluginRegistrant/FlutterPluginRegistrant.podspec
+++ .ios/Flutter/FlutterPluginRegistrant/FlutterPluginRegistrant.podspec
@@ -19,4 +19,7 @@ Depends on all your plugins, and provides a function to register them.
   s.static_framework    = true
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
   s.dependency 'Flutter'
+  s.dependency 'flutter_phone_direct_caller'
+  s.dependency 'share'
+  s.dependency 'url_launcher'
 end
--- lib/dart_commons.dart
+++ lib/dart_commons.dart
@@ -4,19 +4,25 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:logging/logging.dart';
+import 'package:share/share.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// A Calculator.
 
 extension StringAnko on String {
   void log({String className}) {
-    Logger(className != null ? className : 'StringAnko').log(Level.INFO, this);
+    Logger(className != null ? className : 'StringAnko')
+        .log(Level.INFO, this);
   }
 
   void logError({String className}) {
-    Logger(className != null ? className : 'StringAnko').log(Level.SEVERE, this);
+    Logger(className != null ? className : 'StringAnko')
+        .log(Level.SEVERE, this);
   }
 
+//? String to Int
   int get toInt {
     try {
       return int.parse(this);
@@ -26,6 +32,7 @@ extension StringAnko on String {
     }
   }
 
+  //? String to Double
   double get toDouble {
     try {
       return double.parse(this);
@@ -34,6 +41,8 @@ extension StringAnko on String {
       throw ('Invalid Number');
     }
   }
+
+  //? Show Snack BAr with Passed String as Message
 
   void ShowSnackBar(
     BuildContext context, {
@@ -54,15 +63,95 @@ extension StringAnko on String {
     }
   }
 
+  /// ? Convert Filepath to Base64
   String get toBase64 {
     File image = File(this);
     List<int> imageBytes = image.readAsBytesSync();
-    print(imageBytes);
+    Logger('toBase64').info(imageBytes);
     return base64Encode(imageBytes);
+  }
+
+  /// ? Share Passed Text
+  void share({String subject}) {
+    Share.share(this, subject: subject);
+  }
+
+  /// ? Send Whats App Message With the passed String as Message Body
+  void sendWhatsAppMessage({
+    String phone,
+  }) async {
+    String url() {
+      if (Platform.isIOS) {
+        if (phone != null) {
+          return "whatsapp://wa.me/$phone?text=${Uri.parse(this)}";
+        } else {
+          return "whatsapp://wa.me/?text=${Uri.parse(this)}";
+        }
+      } else {
+        if (phone != null) {
+          return "whatsapp://send?phone=$phone&text=${Uri.parse(this)}";
+        } else {
+          return "whatsapp://send?text=${Uri.parse(this)}";
+        }
+      }
+    }
+
+    if (await canLaunch(url())) {
+      await launch(url());
+    } else {
+      throw 'Could not launch ${url()}';
+    }
+  }
+
+  ///
+  /// * Make Phone Call
+  /// ? To Use In Ios Add Add this to your info.plist under dict
+  /// ?<key>LSApplicationQueriesSchemes</key>
+  ///  ?<array>
+  ///     ?<string>tel</string>
+  ///   ?</array>
+
+  void call() async {
+    try {
+      bool res = await FlutterPhoneDirectCaller.callNumber(this);
+      if (!res) {
+        throw ('Invalid Phone Number');
+      }
+    } catch (e) {
+      Logger('StringAnko').severe('Invalid Phone Number : $e');
+    }
+  }
+
+  /// ? Dial Phone Number
+  void dial() async {
+    if (await canLaunch('tel:$this')) {
+      launch('tel:$this');
+    } else {
+      throw ('Invalid Phone Number');
+    }
+  }
+
+  /// ? Browse the passed String as Link
+  void browse() async {
+    if (await canLaunch('$this')) {
+      launch('$this');
+    } else {
+      throw ('Invalid Link');
+    }
+  }
+
+  /// ? Send Email With The Passed String As Email Body
+  void email(String to, {String subject = 'New Mail'}) async {
+    if (await canLaunch('mailto:$to?subject=$subject&body=$this')) {
+      launch('mailto:$to?subject=$subject&body=$this');
+    } else {
+      throw ('Invalid Email Address');
+    }
   }
 }
 
 extension FileAnko on File {
+  ///? Convert File To Base64
   String get toBase64 {
     List<int> imageBytes = this.readAsBytesSync();
     print(imageBytes);
@@ -71,6 +160,7 @@ extension FileAnko on File {
 }
 
 extension ContextAnko on BuildContext {
+  ///? Show SnackBar from build context
   void ShowSnackBar(
     String text, {
     Duration duration = const Duration(milliseconds: 500),
@@ -89,9 +179,20 @@ extension ContextAnko on BuildContext {
       Logger('SnackBar').severe(e);
     }
   }
+
+  log(String message, {String className}) {
+    Logger(className != null ? className : 'StringAnko')
+        .log(Level.INFO, this);
+  }
+
+  logError(String message, {String className}) {
+    Logger(className != null ? className : 'StringAnko')
+        .log(Level.SEVERE, this);
+  }
 }
 
 extension IntExtension on int {
+  /// ? Delay for the passed int value (Specify the required duration [days,hours,minutes,seconds,milliseconds,microseconds])
   delay(
       {Function onCompleted,
       bool days = false,
@@ -112,41 +213,46 @@ extension IntExtension on int {
   }
 
   void log({String className}) {
-    Logger(className != null ? className : 'IntAnko').log(Level.INFO, this.toString());
+    Logger(className != null ? className : 'IntAnko')
+        .log(Level.INFO, this.toString());
   }
 
   void logError({String className}) {
-    Logger(className != null ? className : 'IntAnko').log(Level.SEVERE, this.toString());
+    Logger(className != null ? className : 'IntAnko')
+        .log(Level.SEVERE, this.toString());
   }
 }
 
 extension DoubleAnko on double {
   void log({String className}) {
-    Logger(className != null ? className : 'DoubleAnko').log(Level.INFO, this.toString());
+    Logger(className != null ? className : 'DoubleAnko')
+        .log(Level.INFO, this.toString());
   }
 
   void logError({String className}) {
-    Logger(className != null ? className : 'DoubleAnko').log(Level.SEVERE, this.toString());
+    Logger(className != null ? className : 'DoubleAnko')
+        .log(Level.SEVERE, this.toString());
   }
 }
 
-main() {
-  Logger.root.level = Level.ALL; // defaults to Level.INFO
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  'aaa'.log(className: 'Main');
-  300.delay(
-      onCompleted: () {
-        'entered'.log();
-      },
-      milliseconds: true);
-  print('900'.toInt);
-  print('900'.toDouble);
-//  Scaffold(
-//    body: Builder(builder: (context) {
-//      'asdas'.ShowSnackBar(context);
-//      return Container();
-//    }),
-//  );
+extension CollectionsExtentsions on List {
+  /// ? Add value to list if dosn't exist else replace Current Value
+  void addOrReplace(model) {
+    if (this.contains(model)) {
+      this.insert(this.indexOf(model), model);
+      this.removeAt(this.indexOf(model));
+    } else {
+      this.add(model);
+    }
+  }
+
+  ///? Set Value At index
+  void replace(model, int index) {
+    if (index < this.length) {
+      this.insert(index, model);
+      this.removeAt(this.indexOf(model) + 1);
+    } else {
+      throw ('Index Out Of Bounds');
+    }
+  }
 }
